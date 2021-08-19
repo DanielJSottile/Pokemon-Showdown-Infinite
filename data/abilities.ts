837c0ca0506055e72901bd604cefaff6e0c88571
Daniel Sottile <danielsottile@gmail.com>
2021-08-19 12:00:07 -0400

    added changes to current abilities

--- data/abilities.ts
+++ data/abilities.ts
@@ -4969,8 +4969,8 @@ export const Abilities: { [abilityid: string]: AbilityData } = {
 						secondBestStat = source.storedStats[s];
 					}
 				}
-				this.boost({[statName]: 1}, source);
-				this.boost({[statNameTwo]: 1}, source);
+				this.boost({ [statName]: 1 }, source);
+				this.boost({ [statNameTwo]: 1 }, source);
 			}
 		},
 		name: "Supreme Being",
