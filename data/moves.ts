5e423aa6a0b829ed76339d3faed2d13259ca0ef4
Daniel Sottile <danielsottile@gmail.com>
2021-08-20 22:04:22 -0400

    changes to old moves added

--- data/moves.ts
+++ data/moves.ts
@@ -6188,7 +6188,7 @@ export const Moves: { [moveid: string]: MoveData } = {
 		target: "normal",
 		type: "Water",
 	},
-	fissure: {
+	"fissure": {
 		num: 90,
 		accuracy: 50,
 		basePower: 0,
@@ -9192,7 +9192,7 @@ export const Moves: { [moveid: string]: MoveData } = {
 		zMove: {boost: {spe: 1}},
 		contestType: "Clever",
 	},
-	guillotine: {
+	"guillotine": {
 		num: 12,
 		accuracy: 50,
 		basePower: 0,
@@ -10312,7 +10312,7 @@ export const Moves: { [moveid: string]: MoveData } = {
 		type: "Normal",
 		contestType: "Cool",
 	},
-	horndrill: {
+	"horndrill": {
 		num: 32,
 		accuracy: 50,
 		basePower: 0,
@@ -12444,7 +12444,7 @@ export const Moves: { [moveid: string]: MoveData } = {
 		type: "Dark",
 		contestType: "Clever",
 	},
-	landswrath: {
+	"landswrath": {
 		num: 616,
 		accuracy: 100,
 		basePower: 90,
@@ -16007,9 +16007,9 @@ export const Moves: { [moveid: string]: MoveData } = {
 		priority: 0,
 		flags: {protect: 1, mirror: 1},
 		onTryImmunity(target) {
-			return this.dex.getImmunity("trapped", target);
+			return this.dex.getImmunity('trapped', target);
 		},
-		volatileStatus: "octolock",
+		volatileStatus: 'octolock',
 		condition: {
 			duration: 4,
 			onStart(pokemon, source) {
@@ -16044,18 +16044,9 @@ export const Moves: { [moveid: string]: MoveData } = {
 			onResidualOrder: 11,
 			onResidual(pokemon) {
 				const source = this.effectState.source;
-				if (
-					source &&
-					(!source.isActive || source.hp <= 0 || !source.activeTurns)
-				) {
-					delete pokemon.volatiles["octolock"];
-					this.add(
-						"-end",
-						pokemon,
-						"Octolock",
-						"[partiallytrapped]",
-						"[silent]"
-					);
+				if (source && (!source.isActive || source.hp <= 0 || !source.activeTurns)) {
+					delete pokemon.volatiles['octolock'];
+					this.add('-end', pokemon, 'Octolock', '[partiallytrapped]', '[silent]');
 					return;
 				}
 				this.boost(
@@ -16066,7 +16057,7 @@ export const Moves: { [moveid: string]: MoveData } = {
 				);
 			},
 			onTrapPokemon(pokemon) {
-				if (this.effectState.source && this.effectState.source.isActive) { pokemon.tryTrap(); }
+				if (this.effectState.source && this.effectState.source.isActive) pokemon.tryTrap();
 			},
 		},
 		secondary: null,
@@ -17321,7 +17312,7 @@ export const Moves: { [moveid: string]: MoveData } = {
 		volatileStatus: "powertrick",
 		condition: {
 			onStart(pokemon) {
-				this.add("-start", pokemon, "Power Trick");
+				this.add('-start', pokemon, 'Power Trick');
 				const newatk = pokemon.storedStats.def;
 				const newspa = pokemon.storedStats.spd;
 				const newdef = pokemon.storedStats.atk;
@@ -17342,7 +17333,7 @@ export const Moves: { [moveid: string]: MoveData } = {
 				pokemon.storedStats.spd = newspd;
 			},
 			onEnd(pokemon) {
-				this.add("-end", pokemon, "Power Trick");
+				this.add('-end', pokemon, 'Power Trick');
 				const newatk = pokemon.storedStats.def;
 				const newspa = pokemon.storedStats.spd;
 				const newdef = pokemon.storedStats.atk;
@@ -20048,7 +20039,7 @@ export const Moves: { [moveid: string]: MoveData } = {
 		type: "Psychic",
 		contestType: "Cool",
 	},
-	sheercold: {
+	"sheercold": {
 		num: 329,
 		accuracy: 50,
 		basePower: 0,
