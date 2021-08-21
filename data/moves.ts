f950cfbea5358f3776a28c77dbfa364d3ce41510
Daniel Sottile <danielsottile@gmail.com>
2021-08-20 22:39:24 -0400

    i think thats everything

--- data/moves.ts
+++ data/moves.ts
@@ -14694,13 +14694,19 @@ export const Moves: { [moveid: string]: MoveData } = {
 			onHit(source) {
 				if (!source.volatiles["dynamax"]) return;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 83893be6e... i think thats everything
 				const result = this.random(5);
 				if (result === 0) {
 					this.field.setWeather("sunnyday");
 				}
+<<<<<<< HEAD
 =======
 				this.field.setWeather("sunnyday");
 >>>>>>> 3bbfa5c75... added new moves but not gmax ones
+=======
+>>>>>>> 83893be6e... i think thats everything
 			},
 		},
 		target: "adjacentFoe",
@@ -14746,13 +14752,19 @@ export const Moves: { [moveid: string]: MoveData } = {
 			onHit(source) {
 				if (!source.volatiles["dynamax"]) return;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 83893be6e... i think thats everything
 				const result = this.random(5);
 				if (result === 0) {
 					this.field.setWeather("raindance");
 				}
+<<<<<<< HEAD
 =======
 				this.field.setWeather("raindance");
 >>>>>>> 3bbfa5c75... added new moves but not gmax ones
+=======
+>>>>>>> 83893be6e... i think thats everything
 			},
 		},
 		target: "adjacentFoe",
@@ -14831,13 +14843,19 @@ export const Moves: { [moveid: string]: MoveData } = {
 			onHit(source) {
 				if (!source.volatiles["dynamax"]) return;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 83893be6e... i think thats everything
 				const result = this.random(5);
 				if (result === 0) {
 					this.field.setWeather("hail");
 				}
+<<<<<<< HEAD
 =======
 				this.field.setWeather("hail");
 >>>>>>> 3bbfa5c75... added new moves but not gmax ones
+=======
+>>>>>>> 83893be6e... i think thats everything
 			},
 		},
 		target: "adjacentFoe",
@@ -14883,13 +14901,19 @@ export const Moves: { [moveid: string]: MoveData } = {
 			onHit(source) {
 				if (!source.volatiles["dynamax"]) return;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 83893be6e... i think thats everything
 				const result = this.random(5);
 				if (result === 0) {
 					this.field.setTerrain("electricterrain");
 				}
+<<<<<<< HEAD
 =======
 				this.field.setTerrain("electricterrain");
 >>>>>>> 3bbfa5c75... added new moves but not gmax ones
+=======
+>>>>>>> 83893be6e... i think thats everything
 			},
 		},
 		target: "adjacentFoe",
@@ -14910,13 +14934,19 @@ export const Moves: { [moveid: string]: MoveData } = {
 			onHit(source) {
 				if (!source.volatiles["dynamax"]) return;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 83893be6e... i think thats everything
 				const result = this.random(5);
 				if (result === 0) {
 					this.field.setTerrain("psychicterrain");
 				}
+<<<<<<< HEAD
 =======
 				this.field.setTerrain("psychicterrain");
 >>>>>>> 3bbfa5c75... added new moves but not gmax ones
+=======
+>>>>>>> 83893be6e... i think thats everything
 			},
 		},
 		target: "adjacentFoe",
@@ -14962,13 +14992,19 @@ export const Moves: { [moveid: string]: MoveData } = {
 			onHit(source) {
 				if (!source.volatiles["dynamax"]) return;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 83893be6e... i think thats everything
 				const result = this.random(5);
 				if (result === 0) {
 					this.field.setTerrain("grassyterrain");
 				}
+<<<<<<< HEAD
 =======
 				this.field.setTerrain("grassyterrain");
 >>>>>>> 3bbfa5c75... added new moves but not gmax ones
+=======
+>>>>>>> 83893be6e... i think thats everything
 			},
 		},
 		target: "adjacentFoe",
@@ -15039,13 +15075,19 @@ export const Moves: { [moveid: string]: MoveData } = {
 			onHit(source) {
 				if (!source.volatiles["dynamax"]) return;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 83893be6e... i think thats everything
 				const result = this.random(5);
 				if (result === 0) {
 					this.field.setWeather("sandstorm");
 				}
+<<<<<<< HEAD
 =======
 				this.field.setWeather("sandstorm");
 >>>>>>> 3bbfa5c75... added new moves but not gmax ones
+=======
+>>>>>>> 83893be6e... i think thats everything
 			},
 		},
 		target: "adjacentFoe",
@@ -15066,13 +15108,19 @@ export const Moves: { [moveid: string]: MoveData } = {
 			onHit(source) {
 				if (!source.volatiles["dynamax"]) return;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 83893be6e... i think thats everything
 				const result = this.random(5);
 				if (result === 0) {
 					this.field.setTerrain("mistyterrain");
 				}
+<<<<<<< HEAD
 =======
 				this.field.setTerrain("mistyterrain");
 >>>>>>> 3bbfa5c75... added new moves but not gmax ones
+=======
+>>>>>>> 83893be6e... i think thats everything
 			},
 		},
 		target: "adjacentFoe",
