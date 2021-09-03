485296f020e794fea9a4bfb696337d24839e089c
Daniel Sottile <danielsottile@gmail.com>
2021-09-03 12:24:03 -0400

    courtchange addition

--- data/moves.ts
+++ data/moves.ts
@@ -3626,6 +3626,9 @@ export const Moves: { [moveid: string]: MoveData } = {
 			if (!success) return false;
 			this.add('-activate', source, 'move: Court Change');
 		},
+		secondary: null,
+		target: "all",
+		type: "Normal",
 	},
 	covet: {
 		num: 343,
