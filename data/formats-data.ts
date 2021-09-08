e157c475a46060357f8790fb7cd6a44f0b7283e5
Daniel Sottile <danielsottile@gmail.com>
2021-09-08 10:39:26 -0400

    hopefully this helps the merge

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -2532,7 +2532,7 @@ export const FormatsData: { [k: string]: SpeciesFormatsData } = {
 		randomDoubleBattleMoves: ["brickbreak", "bugbite", "dualwingbeat", "uturn"],
 		randomDoubleBattleLevel: 84,
 		tier: "PU",
-		doublesTier: "LC",
+		doublesTier: "NFE",
 	},
 	scizor: {
 		randomBattleMoves: [
@@ -6099,7 +6099,7 @@ export const FormatsData: { [k: string]: SpeciesFormatsData } = {
 			"snarl",
 			"protect",
 		],
-
+		randomDoubleBattleLevel: 89,
 		tier: "PU",
 		doublesTier: "DUU",
 	},
@@ -15519,15 +15519,7 @@ export const FormatsData: { [k: string]: SpeciesFormatsData } = {
 		doublesTier: "DOU",
 	},
 	zeraora: {
-		randomBattleMoves: [
-			"plasmafists",
-			"closecombat",
-			"voltswitch",
-			"hiddenpowerice",
-			"knockoff",
-			"grassknot",
-			"workup",
-		],
+		randomBattleMoves: ["bulkup", "closecombat", "firepunch", "grassknot", "knockoff", "plasmafists", "playrough", "voltswitch"],
 		randomDoubleBattleMoves: [
 			"plasmafists",
 			"closecombat",
