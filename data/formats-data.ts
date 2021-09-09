bb1c7f39382d779f588bdfb794f6dbf98e212e28
Daniel Sottile <danielsottile@gmail.com>
2021-09-09 19:42:31 -0400

    revert

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -16060,11 +16060,6 @@ export const FormatsData: { [k: string]: SpeciesFormatsData } = {
 		tier: "UUBL",
 		doublesTier: "(DUU)",
 	},
-	// arctozolt: {
-	// 	randomBattleMoves: ["boltbeak", "bulldoze", "iciclecrash", "freezedry"],
-	// 	tier: "NU",
-	// 	doublesTier: "(DUU)",
-	// },
 	dracovish: {
 		randomBattleMoves: ["crunch", "fishiousrend", "icefang", "lowkick", "psychicfangs"],
 		randomBattleLevel: 80,
