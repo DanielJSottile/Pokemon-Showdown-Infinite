21ae7afdc05003252331fb02ef6fdc881a98c810
Daniel Sottile <danielsottile@gmail.com>
2021-09-09 19:41:41 -0400

    please fix merge

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -16060,6 +16060,11 @@ export const FormatsData: { [k: string]: SpeciesFormatsData } = {
 		tier: "UUBL",
 		doublesTier: "(DUU)",
 	},
+	// arctozolt: {
+	// 	randomBattleMoves: ["boltbeak", "bulldoze", "iciclecrash", "freezedry"],
+	// 	tier: "NU",
+	// 	doublesTier: "(DUU)",
+	// },
 	dracovish: {
 		randomBattleMoves: ["crunch", "fishiousrend", "icefang", "lowkick", "psychicfangs"],
 		randomBattleLevel: 80,
